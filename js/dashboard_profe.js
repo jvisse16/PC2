09603a45a3a5a414395af35b94437075fe9f7485
jvisse16 <jvisse16@gmail.com>
2024-04-17 20:13:06 -0500

    primer commit

---
+++ js/dashboard_profe.js
@@ -0,0 +1,9 @@
+
+        function toggleDescription(button) {
+            var description = button.parentElement.querySelector('.description');
+            if (description.style.display === 'block') {
+                description.style.display = 'none';
+            } else {
+                description.style.display = 'block';
+            }
+        }
---
+++ js/login_alumno.js
@@ -0,0 +1,16 @@
+document.querySelector(".login-container form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evitar que se envíe el formulario por defecto
+
+    // Obtener los valores de usuario y contraseña ingresados por el usuario
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Verificar si el nombre de usuario y la contraseña son válidos
+    if (username === "usuario" && password === "123") {
+        // Si son válidos, redirigir al usuario al dashboard
+        window.location.href = "dashboard_alumno.html";
+    } else {
+        // Si no son válidos, mostrar un mensaje de error
+        alert("Nombre de usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
+    }
+});
